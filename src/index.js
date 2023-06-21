3d91126b49432f25e700c4cdd8a2bd9023f1730d
love-to-code108 <love.to.code108@gmail.com>
2023-06-22 00:27:35 +0530

    finally the video part of the website is done plzz take care of the rest of it thank you and good night

--- src/index.js
+++ src/index.js
@@ -1,111 +1,15 @@
-function startVideo(){
-    part5.playVideo();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let player;
-// function onYouTubeIframeAPIReady(){
-//     console.log("api is loaded");
-//     player = YT.Player("player",{
-//         height:500,
-//         width:900,
-//         videoId:"9SbnhgjeyXA&ab",
-//         playerVars:{
-//             playersinline:1,
-//             autoplay:0,
-//             controls:0,
-//         },events:{
-//             // onReady : onPlayerReady ,
-//             // onStateChange : onPlayerStateChange,
-//         }
-//     });
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // <!-- 1. The <iframe> (and video player) will replace this <div> tag. -->
-// //     <div id="player"></div>
-// //     <div id="btn" onclick="startVideo()"></div>
-// //     <script>
-//       // 2. This code loads the IFrame Player API code asynchronously.
-//       var tag = document.createElement('script');
-
-//       tag.src = "https://www.youtube.com/iframe_api";
-//       var firstScriptTag = document.getElementsByTagName('script')[0];
-//       firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-//       // 3. This function creates an <iframe> (and YouTube player)
-//       //    after the API code downloads.
-//       var player;
-//       function onYouTubeIframeAPIReady() {
-//         player = new YT.Player('player', {
-//           height: '390',
-//           width: '640',
-//           videoId: '9SbnhgjeyXA&ab',
-//           playerVars: {
-//             'playsinline': 1
-//           },
-//           events: {
-//             // 'onReady': onPlayerReady,
-//             // 'onStateChange': onPlayerStateChange
-//           }
-//         });
-//       }
+var myVideo = document.getElementById("video1"); 
+
+
+function playPause() { 
+    if (myVideo.paused) 
+      myVideo.play(); 
+    else 
+      myVideo.pause(); 
+  } 
+
+  function hide() {
+    document.getElementById('hidden').hidden = true;
+    document.getElementById('hiddenBtn').hidden = true;
+    myVideo.play();
+ }
